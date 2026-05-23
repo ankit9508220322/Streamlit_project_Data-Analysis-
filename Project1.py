86096a3f6fd4c22a3cff3aa402cf11d649a2f5d8
ankit9508220322 <choubeya503@gmail.com>
2026-05-23 23:03:07 +0530

    change

--- Project1.py
+++ Project1.py
@@ -136,8 +136,6 @@ def All_teame_name(teame_info):
 
         st.title("Team One information ")
         data2["Team1"].value_counts().plot(kind='bar')
-        plt.title("Team1 Count")
-        plt.show()
        
 st.title("IPL OLD Data")
 options=st.sidebar.selectbox('Ipl Old Information Data',['Select options','All ipl data','Select_Year','Highest'])
