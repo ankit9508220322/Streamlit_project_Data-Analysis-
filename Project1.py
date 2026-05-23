5c1abe1a925b84b7b2f1ff2e0f39faf7fa28fe3c
ankit9508220322 <choubeya503@gmail.com>
2026-05-23 18:27:44 +0530

    add dropdown

--- Project1.py
+++ Project1.py
@@ -186,9 +186,19 @@ if btni:
      All_2026_data(ipl_2026)
 
 # ////////////////////////////////// new chapter start
+def ola_information(ola):
+     st.title(ola)
+     pass
 st.title("This project information all Ola ")
 option2=st.sidebar.selectbox('ola information',['select Options','City_name','vehical_Name'])
 if option2 == 'City_name':
+     response=st.sidebar.selectbox(
+          'all info',
+          data2['Season'].drop_duplicates().sort_values(ascending=True)
+     )
+     btn3 = st.sidebar.button("click me")
+     if btn3:
+          ola_information(response)
      st.title("jii")
 elif option2 == "vehical_Name":
      print("not data found ")
